df09022310f0268e9f2e440878f306f60a750c31
Ayen Monasha <161911907+LioMessiForeverKing@users.noreply.github.com>
2025-03-26 16:57:55 -0400

    real data stored

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['i.pravatar.cc', 'i.ibb.co','ibb.co'],
+    domains: ['i.pravatar.cc', 'i.ibb.co','ibb.co', 'your-image-hosting-service.com'],
   }
 
 };
